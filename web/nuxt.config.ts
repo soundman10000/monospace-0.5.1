2ff9a09f129e4c6508d7584f01f6ba06d8158706
Malley <jmalley@goempyrean.com>
2026-09-02 09:31:15 -0500

    correct font import

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -4,6 +4,18 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;1,400&family=Montserrat:wght@400;500;600;800&display=swap',
+        },
+      ],
+    },
+  },
   runtimeConfig: {
     monospaceUrl: 'http://localhost:8100',
     sessionPassword: 'local-web-session-secret-do-not-use-32',
